4b4f4ff9359d93b70381b651a2597b7b7b7c4930
dineshctoula <ds9806026041@gmail.com>
2026-05-15 11:58:22 +0545

    backend second last part

---
+++ backend/dist/src/main.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const common_1 = require("@nestjs/common");
+const core_1 = require("@nestjs/core");
+const app_module_1 = require("./app.module");
+async function bootstrap() {
+    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    app.enableCors({
+        origin: 'http://localhost:3000',
+        credentials: true,
+    });
+    app.useGlobalPipes(new common_1.ValidationPipe({
+        whitelist: true,
+    }));
+    await app.listen(3001);
+    console.log('Server running on port 3001');
+}
+bootstrap();
+//# sourceMappingURL=main.js.map
